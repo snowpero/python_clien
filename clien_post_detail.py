16103e0fa32969a89d2f126b5bba9e619396a3db
Gil-Yong Park <gypark@braincommerce.com>
2016-08-22 17:02:07 +0900

    signature get bug fixed.

--- clien_post_detail.py
+++ clien_post_detail.py
@@ -31,25 +31,29 @@ def getPostDetailData(post_url):
 	c_detail_data.viewinfo = info_txt[0].text
 	c_detail_data.title = title[0].text
 	c_detail_data.text = post_content[0].text
-	c_detail_data.signature = signature[0].text
+	if( len(signature) > 0 ) :
+		c_detail_data.signature = signature[0].text
+	else :
+		c_detail_data.signature = ''
 
 	count = 0
 	reply_items = []
-	for reply_item in reply_list:
-		itemData = ClienDetailReplyItem()
-		itemData.text = reply_item.find_all('div', 'reply_txt scalable')[0].text
-		itemData.date = reply_item.find_all('span', 'reply_date')[0].text
-		isAddReply = False
-		if( str(reply_item).find('reply_add') != -1 ) :
-			isAddReply = True
-		itemData.isAddReply = isAddReply
-		count = count+1
-		json_reply = {
-			'text' : itemData.text,
-			'date' : itemData.date,
-			'isAddReply' : itemData.isAddReply
-		}
-		c_detail_data.arrReplyList.append(json_reply)
+	if( len(reply_list) > 0 ) :
+		for reply_item in reply_list:
+			itemData = ClienDetailReplyItem()			
+			itemData.text = reply_item.find_all('div', 'reply_txt scalable')[0].text
+			itemData.date = reply_item.find_all('span', 'reply_date')[0].text
+			isAddReply = False
+			if( str(reply_item).find('reply_add') != -1 ) :
+				isAddReply = True
+			itemData.isAddReply = isAddReply
+			count = count+1
+			json_reply = {
+				'text' : itemData.text,
+				'date' : itemData.date,
+				'isAddReply' : itemData.isAddReply
+			}
+			c_detail_data.arrReplyList.append(json_reply)
 
 
 	# print 'Add Item Count : ' + str(len(c_detail_data.arrReplyList))
